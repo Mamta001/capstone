ecdaa0a7ab31ec599ded7d40896ceb498c3e4c01
Mamta001 <mamtapourh95@gmail.com>
2021-11-23 17:20:53 -0500

    first commit:

---
+++ include/footer.php
@@ -0,0 +1,33 @@
+<!-- Footer -->
+
+<div class="footer-dark">
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>Services</h3>
+                        <ul>
+                            <li><a href="http://localhost/DreamTeam_Capstone/capstone.github.io/">Compilers</a></li>
+                            <li><a href="http://localhost/DreamTeam_Capstone/capstone.github.io/">Tutorials</a></li>
+                            <li><a href="http://localhost/DreamTeam_Capstone/capstone.github.io/contact.php">Conatct Us</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>About</h3>
+                        <ul>
+                            <li><a href="#">Dream Team</a></li>
+                            <li><a href="http://localhost/DreamTeam_Capstone/capstone.github.io/about.php">About Us</a></li>
+                            <li><a href="http://localhost/DreamTeam_Capstone/capstone.github.io/contact.php">Contact Us</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6 item text">
+                        <h3>Dream Team</h3>
+                        <p>Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit pulvinar dictum vel in justo.</p>
+                    </div>
+                    
+                </div>
+                <p class="copyright">Dream Team © 2021</p>
+            </div>
+        </footer>
+    </div>
+
